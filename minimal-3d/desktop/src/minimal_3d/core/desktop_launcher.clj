a5bd25f64c6007a827a6aac0134f2c49b577e031
oakes <zsoakes@gmail.com>
2014-01-29 19:34:16 -0500

    Add minimal-3d

---
+++ minimal-3d/desktop/src/minimal_3d/core/desktop_launcher.clj
@@ -0,0 +1,10 @@
+(ns minimal-3d.core.desktop-launcher
+  (:require [minimal-3d.core :refer :all])
+  (:import [com.badlogic.gdx.backends.lwjgl LwjglApplication]
+           [org.lwjgl.input Keyboard])
+  (:gen-class))
+
+(defn -main
+  []
+  (LwjglApplication. minimal-3d "minimal-3d" 800 600 true)
+  (Keyboard/enableRepeatEvents true))
---
+++ minimal-3d/ios/src/java/minimal_3d/core/IOSLauncher.java
@@ -0,0 +1,31 @@
+package minimal_3d.core;
+
+import clojure.lang.RT;
+import clojure.lang.Symbol;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.badlogic.gdx.Game;
+
+import org.robovm.cocoatouch.foundation.NSAutoreleasePool;
+import org.robovm.cocoatouch.uikit.UIApplication;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+	protected IOSApplication createApplication() {
+		IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+          RT.var("clojure.core", "require").invoke(Symbol.intern("minimal-3d.core"));
+		try {
+			Game game = (Game) RT.var("minimal-3d.core", "minimal-3d").deref();
+			return new IOSApplication(game, config);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void main(String[] argv) {
+		NSAutoreleasePool pool = new NSAutoreleasePool();
+		UIApplication.main(argv, null, IOSLauncher.class);
+		pool.drain();
+	}
+}
