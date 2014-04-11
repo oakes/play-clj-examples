9a22801b046d8c41ca1edc4e398cb33282b7b04c
Zach Oakes <zsoakes@gmail.com>
2014-04-11 17:05:30 -0400

    Update for RoboVM 0.0.11

--- dungeon-crawler/ios/src/java/dungeon_crawler/core/IOSLauncher.java
+++ dungeon-crawler/ios/src/java/dungeon_crawler/core/IOSLauncher.java
@@ -3,17 +3,16 @@ package dungeon_crawler.core;
 import clojure.lang.RT;
 import clojure.lang.Symbol;
 
-import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
-import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.backends.iosrobovm.*;
+import com.badlogic.gdx.*;
 
-import org.robovm.cocoatouch.foundation.NSAutoreleasePool;
-import org.robovm.cocoatouch.uikit.UIApplication;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.uikit.*;
 
 public class IOSLauncher extends IOSApplication.Delegate {
 	protected IOSApplication createApplication() {
 		IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-          RT.var("clojure.core", "require").invoke(Symbol.intern("dungeon-crawler.core"));
+		RT.var("clojure.core", "require").invoke(Symbol.intern("dungeon-crawler.core"));
 		try {
 			Game game = (Game) RT.var("dungeon-crawler.core", "dungeon-crawler").deref();
 			return new IOSApplication(game, config);
@@ -26,6 +25,6 @@ public class IOSLauncher extends IOSApplication.Delegate {
 	public static void main(String[] argv) {
 		NSAutoreleasePool pool = new NSAutoreleasePool();
 		UIApplication.main(argv, null, IOSLauncher.class);
-		pool.drain();
+		pool.close();
 	}
 }
