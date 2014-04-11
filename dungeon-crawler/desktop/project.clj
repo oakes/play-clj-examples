290100cfbc50cd2b5252be7072d26ed1b3ede7f5
oakes <zsoakes@gmail.com>
2014-04-11 16:40:19 -0400

    Update to Clojure 1.6

--- dungeon-crawler/desktop/project.clj
+++ dungeon-crawler/desktop/project.clj
@@ -5,7 +5,7 @@
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "1.0-SNAPSHOT"]
                  [com.badlogicgames.gdx/gdx-platform "1.0-SNAPSHOT"
                   :classifier "natives-desktop"]
-                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure "1.6.0"]
                  [play-clj "0.3.0-SNAPSHOT"]]
   :repositories [["sonatype"
                   "https://oss.sonatype.org/content/repositories/snapshots/"]]
--- dungeon-crawler/ios/project.clj
+++ dungeon-crawler/ios/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :dependencies [[com.badlogicgames.gdx/gdx "1.0-SNAPSHOT"]
                  [com.badlogicgames.gdx/gdx-backend-robovm "1.0-SNAPSHOT"]
-                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure "1.6.0"]
                  [play-clj "0.3.0-SNAPSHOT"]]
   :repositories [["sonatype"
                   "https://oss.sonatype.org/content/repositories/snapshots/"]]
