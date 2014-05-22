b6a7413144ec8609e10e8c6d9dcf6d64bf231647
oakes <zsoakes@gmail.com>
2014-05-22 02:05:10 -0400

    Add position keys

--- minimal-3d/desktop/src-common/minimal_3d/core.clj
+++ minimal-3d/desktop/src-common/minimal_3d/core.clj
@@ -23,8 +23,9 @@
           model-mat (material :set attr)
           model-attrs (bit-or (usage :position) (usage :normal))
           builder (model-builder)]
-      (model (model-builder! builder :create-box 2 2 2 model-mat model-attrs)
-             (vector-3 0 0 0))))
+      (-> (model-builder! builder :create-box 2 2 2 model-mat model-attrs)
+          model
+          (assoc :x 1 :y 0 :z 0))))
   :on-render
   (fn [screen entities]
     (clear! 1 1 1 1)
