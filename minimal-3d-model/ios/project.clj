bd54ee6c77fada1e619eeb5b88e8d9af6ffbd303
oakes <zsoakes@gmail.com>
2014-08-26 18:00:14 -0400

    Update to 0.3.10-SNAPSHOT

--- minimal-3d-model/ios/project.clj
+++ minimal-3d-model/ios/project.clj
@@ -1,11 +1,11 @@
 (defproject minimal-3d-model "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[com.badlogicgames.gdx/gdx "1.3.0"]
-                 [com.badlogicgames.gdx/gdx-backend-robovm "1.3.0"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.3.0"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.3.0"]
+  :dependencies [[com.badlogicgames.gdx/gdx "1.3.1"]
+                 [com.badlogicgames.gdx/gdx-backend-robovm "1.3.1"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.3.1"]
+                 [com.badlogicgames.gdx/gdx-bullet "1.3.1"]
                  [org.clojure/clojure "1.6.0"]
-                 [play-clj "0.3.9"]]
+                 [play-clj "0.3.10-SNAPSHOT"]]
   :source-paths ["src/clojure" "../desktop/src-common"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
