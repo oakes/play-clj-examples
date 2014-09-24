fedf5c67d821ff12bcae76098008bc8e04759507
oakes <zsoakes@gmail.com>
2014-09-24 16:21:38 -0400

    Update play-clj to 0.4.0

--- minicraft-online/desktop/project.clj
+++ minicraft-online/desktop/project.clj
@@ -12,7 +12,7 @@
                  [com.badlogicgames.gdx/gdx-platform "1.3.1"
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.6.0"]
-                 [play-clj "0.4.0-SNAPSHOT"]
+                 [play-clj "0.4.0"]
                  [play-clj.net "0.1.2"]
                  [prismatic/schema "0.2.6"]]
   
