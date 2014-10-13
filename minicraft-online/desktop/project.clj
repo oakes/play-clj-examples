35138d3095d9445b361c8dafda50631457ad8157
oakes <zsoakes@gmail.com>
2014-10-12 21:40:30 -0400

    Use play-clj 0.4.1-SNAPSHOT

--- minicraft-online/desktop/project.clj
+++ minicraft-online/desktop/project.clj
@@ -1,18 +1,18 @@
 (defproject minicraft-online "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   
-  :dependencies [[com.badlogicgames.gdx/gdx "1.3.1"]
-                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.3.1"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.3.1"]
-                 [com.badlogicgames.gdx/gdx-box2d-platform "1.3.1"
+  :dependencies [[com.badlogicgames.gdx/gdx "1.4.1"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.4.1"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.4.1"]
+                 [com.badlogicgames.gdx/gdx-box2d-platform "1.4.1"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.3.1"]
-                 [com.badlogicgames.gdx/gdx-bullet-platform "1.3.1"
+                 [com.badlogicgames.gdx/gdx-bullet "1.4.1"]
+                 [com.badlogicgames.gdx/gdx-bullet-platform "1.4.1"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-platform "1.3.1"
+                 [com.badlogicgames.gdx/gdx-platform "1.4.1"
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.6.0"]
-                 [play-clj "0.4.0"]
+                 [play-clj "0.4.1-SNAPSHOT"]
                  [play-clj.net "0.1.2"]
                  [prismatic/schema "0.2.6"]]
   
--- minicraft-online/ios/project.clj
+++ minicraft-online/ios/project.clj
@@ -1,11 +1,11 @@
 (defproject minicraft-online "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[com.badlogicgames.gdx/gdx "1.3.1"]
-                 [com.badlogicgames.gdx/gdx-backend-robovm "1.3.1"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.3.1"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.3.1"]
+  :dependencies [[com.badlogicgames.gdx/gdx "1.4.1"]
+                 [com.badlogicgames.gdx/gdx-backend-robovm "1.4.1"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.4.1"]
+                 [com.badlogicgames.gdx/gdx-bullet "1.4.1"]
                  [org.clojure/clojure "1.6.0"]
-                 [play-clj "0.4.0"]
+                 [play-clj "0.4.1-SNAPSHOT"]
                  [play-clj.net "0.1.0"]
                  [prismatic/schema "0.2.6"]]
   :source-paths ["src/clojure" "../desktop/src-common"]
