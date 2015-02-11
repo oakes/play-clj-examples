a712737bd453d3c85eaf4517aa2e6f8b8f09ca29
oakes <zsoakes@gmail.com>
2015-02-11 14:56:57 -0500

    Update play-clj to 0.4.4-SNAPSHOT

--- minicraft-online/ios/project.clj
+++ minicraft-online/ios/project.clj
@@ -1,11 +1,11 @@
 (defproject minicraft-online "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[com.badlogicgames.gdx/gdx "1.5.0"]
-                 [com.badlogicgames.gdx/gdx-backend-robovm "1.5.0"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.5.0"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.5.0"]
+  :dependencies [[com.badlogicgames.gdx/gdx "1.5.3"]
+                 [com.badlogicgames.gdx/gdx-backend-robovm "1.5.3"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.5.3"]
+                 [com.badlogicgames.gdx/gdx-bullet "1.5.3"]
                  [org.clojure/clojure "1.6.0"]
-                 [play-clj "0.4.3"]
+                 [play-clj "0.4.4-SNAPSHOT"]
                  [play-clj.net "0.1.0"]
                  [prismatic/schema "0.2.6"]]
   :source-paths ["src/clojure" "../desktop/src-common"]
