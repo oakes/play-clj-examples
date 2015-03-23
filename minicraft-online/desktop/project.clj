ad1c5423d9d995a728b79edcf6133d4e313664f5
oakes <zsoakes@gmail.com>
2015-03-22 23:52:05 -0400

    Update play-clj to 0.4.6

--- minicraft-online/desktop/project.clj
+++ minicraft-online/desktop/project.clj
@@ -12,7 +12,7 @@
                  [com.badlogicgames.gdx/gdx-platform "1.5.5"
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.7.0-alpha5"]
-                 [play-clj "0.4.5"]
+                 [play-clj "0.4.6"]
                  [play-clj.net "0.1.2"]
                  [prismatic/schema "0.2.6"]]
   
