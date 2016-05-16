0cbe836b9967752505be3f7c44f3dc32e58e5532
oakes <zsoakes@gmail.com>
2016-05-16 17:57:32 -0400

    Update libGDX to 1.9.3 and play-clj to 1.1.0-SNAPSHOT

--- minicraft-online/desktop/project.clj
+++ minicraft-online/desktop/project.clj
@@ -1,18 +1,18 @@
 (defproject minicraft-online "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   
-  :dependencies [[com.badlogicgames.gdx/gdx "1.8.0"]
-                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.8.0"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.8.0"]
-                 [com.badlogicgames.gdx/gdx-box2d-platform "1.8.0"
+  :dependencies [[com.badlogicgames.gdx/gdx "1.9.3"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.9.3"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.9.3"]
+                 [com.badlogicgames.gdx/gdx-box2d-platform "1.9.3"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.8.0"]
-                 [com.badlogicgames.gdx/gdx-bullet-platform "1.8.0"
+                 [com.badlogicgames.gdx/gdx-bullet "1.9.3"]
+                 [com.badlogicgames.gdx/gdx-bullet-platform "1.9.3"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-platform "1.8.0"
+                 [com.badlogicgames.gdx/gdx-platform "1.9.3"
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.7.0"]
-                 [play-clj "1.0.0"]
+                 [play-clj "1.1.0-SNAPSHOT"]
                  [play-clj.net "0.1.2"]
                  [prismatic/schema "0.2.6"]]
   
